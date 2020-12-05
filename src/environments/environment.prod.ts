7141ad2fe63d557bc0f34375a5f1ba84e31591e5
angela.fonseca <angela.fonseca@leucotron.com.br>
2020-12-04 21:40:02 -0300

    Config heroku deploy.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  pokemonURL: 'https://pokeapi.co/api/v2/pokemon',
+  pokemonSpeciesURL: 'https://pokeapi.co/api/v2/pokemon-species',
+  pokemonGenerationURL: 'https://pokeapi.co/api/v2/generation'
 };
